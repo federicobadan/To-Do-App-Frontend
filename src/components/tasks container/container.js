95186fed2a6a268a93d8b68eace7e97c85f83cfd
Federico <federicobadan@hotmail.com~>
2023-06-25 17:15:31 -0300

    still working

--- src/components/tasks container/container.js
+++ src/components/tasks container/container.js
@@ -1,47 +1,70 @@
 import './container.css';
-import Tasks from '../tasks/tasks.js';
-import Submit from '../tasks submit/submit.js';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import axios, * as others from 'axios';
+import Tasks from '../tasks/tasks';
+import Submit from '../tasks submit/submit';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import xtype from 'xtypejs';
+    
 
 export default function Container() {
     
-    const [tasks, setTasks] = useState([]);
-    const getTasks = async () => {
-        let response = null;
-        try{
-            response = await axios.get('http://localhost:3001/tasks');
-        }
-        catch (error) {
-            console.error(error);
-        }
-        finally {
-            setTasks(response.data);
-        }
-    };
+    // const [tasks, setTasks] = useState();
+    // const [error, setError] = useState();
+    // const getTasks = async () => {
+    //     let response = [];
+    //     try{
+    //         response = await axios.get('http://localhost:3001/tasks');
+    //     }
+    //     catch (error) {
+    //         console.error(error.toJSON());
+    //         setError(error.toJSON());
+    //     }
+    //     finally {
+    //         setTasks(response.data);
+    //     }
+    // };
 
-    useEffect(() => {
-        getTasks();
-      }, []);
-
+    // useEffect(() => {
+    //     getTasks();
+    // }, []); 
+    console.log()
+    // const tasks = useFetch ({url:'http://localhost:3001/tasks'});
+    const [data, setData] = useState();
+    const getData = (options) => {
+        useEffect (()=>{
+            fetch(options.url)
+            .then((response) => response.json)
+            .then((json)=>setData(json))
+        }, []);
+        return {
+            data,
+        };
+    }
+    const tasks = getData({url:'http://localhost:3001/tasks'});
     return (
         <>
-            <div className="title">
-                <h1>To do App</h1>
-            </div>
-            <div className='container'>
-                <Submit />
-                <div className="container-tasks">
-                    
-                    {tasks.map( (x) => (
-                        
-                        <Tasks key={x.id} complete={x.complete} task={x.task} created={x.created_date} />
-                    )
-                    )}
+            {error ? 
+            <>
+            <p>Aplicación fuera de servicio</p>
+            <p>Código: {error.code}</p>
+            </>: null}
+            {tasks ? 
+            <>
+                <div className="title">
+                    <h1>To do App</h1>
                 </div>
-            </div>
+                <div className='container'>
+                    <Submit />
+                    <div className="container-tasks">
+                        
+                        {tasks.map( (x) => (
+                            <Tasks key={x.id} id={x.id} flag={x.flag} task={x.task} created={x.created_date} />
+                            )
+                        )}
+
+                    </div>
+                </div>
+            </>: null}
         </>
     )
-
 };
--- src/components/tasks/tasks.js
+++ src/components/tasks/tasks.js
@@ -1,10 +1,64 @@
 import './tasks.css';
-export default function Tasks({complete, task, created}) {
+//import { useState} from 'react';
+//import { useEffect } from 'react';
+import axios from 'axios';
+
+export default function Tasks({id, flag, task, created}) {
+
+    //const [completed, setCompleted] = useState(flag);
+    let completed = flag;
+    
+    const updateCompleted = async () => {
+        let response = null;
+        try{
+            response = await axios.patch(`http://localhost:3001/task/${id}`, { flag: completed });
+        }
+        catch (error) {
+            console.error(error);
+        }
+        finally {
+            console.log(response.data)
+        }
+    };
+
+    const  handleCheckbox = () => {
+        //setCompleted(!completed);
+        completed = !completed;
+        updateCompleted();
+    }
+
+    // useEffect(() => {
+    //     updateCompleted();
+    //     console.log(`useEffect update`)
+    //   }, [completed]);
+
+    const editTask = () => {
+
+    }
+
+
+    const deleteTask = () => {
+        created = [];
+    }
+    // const deleteComponent = async () => {
+    //     let response=null;
+    //     try{
+    //      response = await axios.delete(`http://localhost:3001/task/${id}`)
+    //     }
+    //     catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+    // useEffect(() => {
+    //     deleteComponent();
+    //   }, [created]);
+
     return (
         <>
             <div className="tasks">
+
                 <div className="div-checkbox">
-                    <input type="checkbox" name="myCheckbox" defaultChecked={complete} />
+                    <input type="checkbox" name="myCheckbox" defaultChecked={completed} onClick={handleCheckbox} />
                 </div>
 
                 <div className="div-task">
@@ -12,8 +66,8 @@ export default function Tasks({complete, task, created}) {
                 </div>
 
                 <div className="div-buttons">
-                    <button>Editar</button>
-                    <button>Borrar</button>
+                    <button onClick={editTask()}>Editar</button>
+                    <button onClick={deleteTask()}>Borrar</button>
                 </div>
             </div>
         </>
